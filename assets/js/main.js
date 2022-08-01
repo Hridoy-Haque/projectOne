161db45465dea56231c19ac7b0a692348f2adb40
Hridoy <iamnirob334@gmail.com>
2022-08-01 21:41:22 +0600

    change javascript formate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,135 +1,102 @@
-"use strict"
+"use strict";
 
+window.addEventListener("load", function () {
+  /*=========== preloader animaton =================*/
 
+  const loader = document.getElementById("preloader");
+  loader.classList.add("hide_preloader");
 
-/*=========== preloader animaton =================*/
+  /*===========SELECT NAVBAR ELEMENT =================*/
 
-window.addEventListener('load',function(){
-  const loader = document.getElementById('preloader');
-  loader.classList.add('hide_preloader')
-})
+  const header = document.querySelector(".header"),
+    navbar = document.querySelector(".navbar"),
+    showBtn = document.getElementById("showBtn"),
+    closeBtn = document.getElementById("closeBtn");
 
-/*===========SELECT NAVBAR ELEMENT =================*/
+  // mobile navbar active function ======>
 
-const header = document.querySelector('.header'),
-             navbar = document.querySelector('.navbar'),
-             showBtn = document.getElementById('showBtn'),
-             closeBtn = document.getElementById('closeBtn');
+  const navbar_menu = document.getElementById("navbar_menu"),
+    menu_button = document.querySelector(".menu_button");
 
-// mobile navbar active function ======>
-             
-const navbar_menu = document.getElementById("navbar_menu"),
-          menu_button = document.querySelector('.menu_button');
-
-
-    function showNavbar(){
-      navbar_menu.classList.toggle('show_collapse');
-      menu_button.classList.toggle('close_button')
-    }
-    
-    if(menu_button){
-      menu_button.addEventListener('click', showNavbar);
-    }
-
-// navbar background while scroll function ======>
-
-function navbarBg(){
-  let scrollTop = window.scrollY
-  if(scrollTop > 20){
-    header.classList.add('whiteBg')
-  }else{
-    header.classList.remove('whiteBg')
+  function showNavbar() {
+    navbar_menu.classList.toggle("show_collapse");
+    menu_button.classList.toggle("close_button");
   }
-}
 
-window.addEventListener('scroll',navbarBg)
-window.addEventListener('load',navbarBg)
+  if (menu_button) {
+    menu_button.addEventListener("click", showNavbar);
+  }
 
+  // navbar background while scroll function ======>
 
-// swipper for carousel or slider ======>
+  function navbarBg() {
+    let scrollTop = window.scrollY;
+    if (scrollTop > 20) {
+      header.classList.add("whiteBg");
+    } else {
+      header.classList.remove("whiteBg");
+    }
+  }
 
+  window.addEventListener("scroll", navbarBg);
+  window.addEventListener("load", navbarBg);
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
-
-// animation section when page scroll  ======>
-
+  // animation section when page scroll  ======>
 
   function aos_init() {
     AOS.init({
       duration: 1000,
       easing: "ease-in-out",
       once: true,
-      mirror: false
+      mirror: false,
+    });
+  }
+  aos_init();
+
+  // isotope portfolio image gallery  ======>
+
+  let projectContainer = document.querySelector(".project-content");
+
+  if (projectContainer) {
+    let portfolioIsotope = new Isotope(projectContainer, {
+      itemSelector: ".item",
+    });
+
+    let items = document.querySelectorAll(".portfolio_items li");
+
+    items.forEach((item) => {
+      item.addEventListener("click", function (e) {
+        items.forEach((el) => {
+          el.classList.remove("active");
+        });
+        e.target.classList.add("active");
+        portfolioIsotope.arrange({
+          filter: this.getAttribute("data-filter"),
+        });
+        // aos_init();
+      });
     });
   }
 
-  window.addEventListener('load', () => {
-    aos_init();
-  });
+  // scroll to top ========>
 
-// isotope portfolio image gallery  ======>
-  
-window.addEventListener('load', function(){
-  let projectContainer = document.querySelector('.project-content');
+  const scrollBtn = document.querySelector(".scrollTopBtn");
 
-    if (projectContainer) {
-      let portfolioIsotope = new Isotope(projectContainer, {
-        itemSelector: '.item'
-      });
-
-    let items = document.querySelectorAll('.portfolio_items li');
-    
-    items.forEach((item)=>{
-      item.addEventListener('click',function(e){
-        items.forEach((el)=>{
-          el.classList.remove('active')
-        })
-        e.target.classList.add('active')
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        // aos_init();
-      })
-    })
-    
-
+  function scrollButtonActivate() {
+    let scrollTop = window.scrollY;
+    if (scrollTop > 70) {
+      scrollBtn.classList.add("showBtn");
+    } else {
+      scrollBtn.classList.remove("showBtn");
     }
-})
-
-
-
-
-// scroll to top ========>
-
-const scrollBtn = document.querySelector('.scrollTopBtn');
-
-function scrollButtonActivate(){
-  let scrollTop = window.scrollY ;
-  if(scrollTop > 70){
-    scrollBtn.classList.add('showBtn')
-  }else{
-    scrollBtn.classList.remove('showBtn')
   }
-}
+  window.addEventListener("scroll", scrollButtonActivate);
+  scrollButtonActivate();
 
+  // scroll to top when click ========>
 
-window.addEventListener('scroll',scrollButtonActivate)
-window.addEventListener('load',scrollButtonActivate)
-
-scrollBtn.addEventListener('click',function(){
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0
-})
+  scrollBtn.addEventListener("click", function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+});
